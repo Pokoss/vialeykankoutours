3a6c64a4aeaa21a6dd37cb662b3a6819eda384b2
judevercetti <judevercetti@gmail.com>
2023-06-18 08:14:26 +0300

    meta tags added

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,14 +14,14 @@
     <meta property="og:url" content="https://vialeykankoutours.com" />
     <meta property="og:title" content="Vialey Kankou Tours" />
     <meta property="og:description" content="Your gateway to extraordinary adventures in Uganda. Explore breathtaking landscapes, vibrant cultures, and unforgettable wildlife encounters. Let us guide you through the beauty and warmth of Uganda for an unforgettable journey." />
-    <meta property="og:image" content="/Images/web/elephants.jpeg" />
+    <meta property="og:image" content="https://vialeykankoutours.com/Images/web/elephants.jpeg" />
 
     <!-- Twitter -->
     <meta property="twitter:card" content="summary_large_image" />
     <meta property="twitter:url" content="https://vialeykankoutours.com" />
     <meta property="twitter:title" content="Vialey Kankou Tours" />
     <meta property="twitter:description" content="Your gateway to extraordinary adventures in Uganda. Explore breathtaking landscapes, vibrant cultures, and unforgettable wildlife encounters. Let us guide you through the beauty and warmth of Uganda for an unforgettable journey." />
-    <meta property="twitter:image" content="/Images/web/elephants.jpeg" />
+    <meta property="twitter:image" content="https://vialeykankoutours.com/Images/web/elephants.jpeg" />
 
     @vite('resources/js/app.js')
     @inertiaHead
